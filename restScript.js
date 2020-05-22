20a28d490b206497ab14eace62d84d91d2c33c9b
Krystsina Kavalevich <krikoax@gmail.com>
2020-05-22 20:32:38 +0800

    carousel infinite added

--- restScript.js
+++ restScript.js
@@ -7,20 +7,34 @@ const nextBtn = document.querySelector('.btn-light');
 
 //Counter
 let counter = 1;
-const size = carouselImages[0].clientWidth; 
+// const size = carouselImages[0].clientWidth; 
+const size = 1600;
 
 carouselSlide.style.transform = 'translateX('+ (- size * counter) + 'px)';
 
-nextBtn.addEventListener('click', (event) => {
-    event.preventDefault();
-    if (counter >= carouselImages.lenght -1) return;
-    carouselSlide.style.transition = 'transform 0.4s ease-in-out';
-    counter++;
-    carouselSlide.style.transform = 'translateX('+ (- size * counter) +'px)';
-});
+let interval = setInterval( () =>{
+    let timeoutId = setTimeout(() => {
+        // event.preventDefault();
+        if (counter >= carouselImages.lenght - 2) return;
+        carouselSlide.style.transition = 'transform 0.4s ease-in-out';
+        counter++;
+        carouselSlide.style.transform = 'translateX('+ (- size * counter) +'px)';
+        console.log(counter);
+        console.log(carouselImages);
+    }, 5000);
+}, 5000);
+
+// nextBtn.addEventListener('click', (event) => {
+//     event.preventDefault();
+//     if (counter > carouselImages.lenght - 2) return;
+//     carouselSlide.style.transition = 'transform 0.4s ease-in-out';
+//     counter++;
+//     carouselSlide.style.transform = 'translateX('+ (- size * counter) +'px)';
+//     console.log(counter);
+// });
 prevBtn.addEventListener('click', (event) => {
     event.preventDefault();
-    if (counter < 0) return;
+    if (counter <= 0) return;
     carouselSlide.style.transition = 'transform 0.4s ease-in-out';
     counter--;
     carouselSlide.style.transform = 'translateX('+ (- size * counter) +'px)';
@@ -32,7 +46,9 @@ carouselSlide.addEventListener('transitionend', () => {
 	if(carouselImages[counter].id === 'lastClone') {
 		carouselSlide.style.transition = 'none';
 		counter = carouselImages.length - 2;
-		carouselSlide.style.transform = 'translateX('+ (- size * counter) + 'px)';
+        carouselSlide.style.transform = 'translateX('+ (- size * counter) + 'px)';
+        console.log(counter);
+
 	}
 
 	if(carouselImages[counter].id === 'firstClone') {
